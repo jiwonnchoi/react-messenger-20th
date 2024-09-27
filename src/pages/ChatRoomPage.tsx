3d84754804d7a12c08d5b19a91df3aca609b7636
JiwonChoi0805 <won0805@ewhain.net>
2024-09-27 13:53:00 +0900

    feat: 모바일 뷰 높이 조정

--- src/pages/ChatRoomPage.tsx
+++ src/pages/ChatRoomPage.tsx
@@ -1,14 +1,28 @@
+import { useState, useEffect } from "react";
 import TopBar from "../components/ChatRoom/TopBar";
 import Chattings from "../components/ChatRoom/Chattings";
 import { useRecoilValue } from "recoil";
 import { emotionBoxState } from "../recoil/atom";
+import { isMobile } from "react-device-detect";
 
 const ChatRoomPage = () => {
   const showEmotionBox = useRecoilValue(emotionBoxState);
+  const [height, setHeight] = useState(isMobile ? "100vh" : "733px");
+
+  useEffect(() => {
+    const handleHeight = () => {
+      setHeight(isMobile ? `${window.innerHeight}px` : "733px");
+    };
+    handleHeight(); // 처음 로드 시 높이 설정
+    window.addEventListener("resize", handleHeight); // 창 크기 변경 시 높이 업데이트
+    return () => {
+      window.removeEventListener("resize", handleHeight);
+    };
+  }, []);
 
   return (
     <>
-      <div className="relative h-[733px] w-[375px] bg-white">
+      <div className="relative w-[375px] bg-white" style={{ height }}>
         <TopBar />
         <div className="overflow-y-auto">
           <Chattings />
