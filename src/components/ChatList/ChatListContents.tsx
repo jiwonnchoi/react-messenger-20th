36b76b929fb54ef09773b745fb07bc7ab51f07df
JiwonChoi0805 <won0805@ewhain.net>
2024-11-02 11:39:26 +0900

    feat: 채팅방 핀 구현

--- src/components/ChatList/ChatListContents.tsx
+++ src/components/ChatList/ChatListContents.tsx
@@ -14,50 +14,75 @@ import userData from "../../data/UserData.json";
 const ChatListContents = () => {
   const chattings = useRecoilValue(chattingState);
   const user = useRecoilValue(userState);
-
   const myId = user.me.id;
-  // const chattingFollowers = chattings.filter((chat) =>
-  //   chat.users.some((id) => id !== myId),
-  // );
 
   const [searchInput, setSearchInput] = useState("");
+  const [pinnedChatIds, setPinnedChatIds] = useState<number[]>([]); // 고정된 채팅 id 배열
+
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchInput(e.target.value);
   };
 
-  const filteredFollowers = chattings.filter((chat) =>
-    chat.users.some((userId) => {
-      if (userId !== myId) {
-        const otherUser = userData.users.find((user) => user.id === userId);
-        return otherUser?.userName.toLocaleLowerCase().includes(searchInput);
-      }
-      return false;
-    }),
-  );
+  const handlePinToggle = (id: number | undefined) => {
+    if (id !== undefined) {
+      setPinnedChatIds(
+        (prevIds) =>
+          prevIds.includes(id)
+            ? prevIds.filter((chatId) => chatId !== id) // 이미 고정된 경우 해제
+            : [id, ...prevIds], // 새로운 고정 항목은 가장 상단으로 추가
+      );
+    }
+  };
+
+  const filteredFollowers = chattings
+    .filter((chat) =>
+      chat.users.some((userId) => {
+        if (userId !== myId) {
+          const otherUser = userData.users.find((user) => user.id === userId);
+          return otherUser?.userName.toLocaleLowerCase().includes(searchInput);
+        }
+        return false;
+      }),
+    )
+    .sort((a, b) => {
+      // 고정된 항목 상단으로 + 최신 항목이 더 위에 오도록 정렬
+      const aIdx = pinnedChatIds.indexOf(a.id!);
+      const bIdx = pinnedChatIds.indexOf(b.id!);
+      if (aIdx === -1 && bIdx === -1) return 0; // 모두 핀 되지 않은 경우 -> 순서 변경 안함
+      if (aIdx === -1) return 1; // a 핀x, b 핀o
+      if (bIdx === -1) return -1; // a 핀o, b 핀x
+      return aIdx - bIdx; // 둘다 핀 된 경우 -> 배열 순대로
+    });
 
   return (
-    <>
-      <div className="px-4">
-        <div className="mb-5 flex h-[2.25rem] w-full flex-row items-center gap-[0.62rem] rounded-[0.625rem] border-[1px] border-solid border-Gray600 px-2 py-[0.62rem]">
-          <SearchIcon className="h-6 w-6" />
-          <input
-            placeholder="Search"
-            className="grow"
-            value={searchInput}
-            onChange={handleInput}
-          />
-        </div>
-        <div className="flex w-full flex-row items-center justify-between pb-[0.44rem]">
-          <span className="text-xl font-medium">Messages</span>
-          <span className="text-xs text-Gray500">Requests</span>
-        </div>
-        <div>
-          {filteredFollowers.map((chatting) => (
-            <ChatListItem key={chatting.id} chatting={chatting} />
-          ))}
-        </div>
+    <div className="px-4">
+      <div className="mb-5 flex h-[2.25rem] w-full flex-row items-center gap-[0.62rem] rounded-[0.625rem] border-[1px] border-solid border-Gray600 px-2 py-[0.62rem]">
+        <SearchIcon className="h-6 w-6" />
+        <input
+          placeholder="Search"
+          className="grow"
+          value={searchInput}
+          onChange={handleInput}
+        />
       </div>
-    </>
+      <div className="flex w-full flex-row items-center justify-between pb-[0.44rem]">
+        <span className="text-xl font-medium">Messages</span>
+        <span className="text-xs text-Gray500">Requests</span>
+      </div>
+      <div>
+        {filteredFollowers.map(
+          (chatting) =>
+            chatting.id !== undefined && ( // id가 undefined가 아닌 항목만 렌더링
+              <ChatListItem
+                key={chatting.id}
+                chatting={chatting}
+                isPinned={pinnedChatIds.includes(chatting.id)}
+                handleTogglePin={() => handlePinToggle(chatting.id)}
+              />
+            ),
+        )}
+      </div>
+    </div>
   );
 };
 
--- src/components/ChatList/ChatListItem.tsx
+++ src/components/ChatList/ChatListItem.tsx
@@ -2,63 +2,103 @@ import { useNavigate } from "react-router-dom";
 
 // icons
 import { ReactComponent as CameraIcon } from "../../assets/icons/camera_gray.svg";
+import { ReactComponent as PinnedIcon } from "../../assets/icons/pin.svg";
+import { ReactComponent as PinIcon } from "../../assets/icons/pin2.svg";
 
 // data & recoil
 import userData from "../../data/UserData.json";
 import { chattingInterface } from "../../types/interface";
 import useHandleTime from "../../hooks/useHandleTime";
+import { useState } from "react";
 
 interface ChatListItemProps {
   chatting: chattingInterface;
+  isPinned: boolean;
+  handleTogglePin: () => void;
 }
 
-const ChatListItem: React.FC<ChatListItemProps> = ({ chatting }) => {
+const ChatListItem: React.FC<ChatListItemProps> = ({
+  chatting,
+  isPinned,
+  handleTogglePin,
+}) => {
   const followerId = chatting.users.filter((id) => id !== 0)[0];
   const follower = userData.users.filter((user) => user.id === followerId)[0];
 
   const lastMessageContent = chatting.chatList[chatting.chatList.length - 1];
   const lastMessage = lastMessageContent?.message.startsWith("data:image/")
-    ? "이미지가 전송됨" // 이미지 파일일 경우 대체 텍스트
+    ? "이미지가 전송됨" // 이미지 파일일 경우 경로 대신 대체 텍스트
     : lastMessageContent.message;
 
   //
   const { lastModified } = useHandleTime(lastMessageContent);
 
   const navigate = useNavigate();
-
-  // const setCurrentChatting = useSetRecoilState(currentChattingState);
   const gotoChatRoom = () => {
     navigate(`/chatroom/${followerId}`);
   };
 
+  const [isSlided, setIsSlided] = useState(false);
+  const clickCamera = () => {
+    setIsSlided(!isSlided);
+  };
+
+  const clickPin = () => {
+    handleTogglePin();
+    // console.log("Pinned!"); // 콘솔 대신 핀 기능 구현
+    setIsSlided(false);
+  };
+
   return (
-    <>
-      <div className="flex w-full flex-row gap-2 py-2">
-        <img
-          className="h-14 w-14 flex-shrink-0 rounded-full"
-          src={require("../../assets/images/" + follower.profileImg + ".svg")}
-        />
-        <span className="flex w-full flex-row items-center justify-between gap-[1.88rem]">
-          <span
-            onClick={() => gotoChatRoom()}
-            className="flex flex-grow cursor-pointer flex-col"
-          >
-            <div className="text-[0.8125rem] font-semibold tracking-tighter">
-              {follower.userName}
-            </div>
-            <div className="flex flex-row justify-between">
-              <span className="h-[19.5px] w-[163px] overflow-hidden text-ellipsis whitespace-nowrap text-[0.8125rem] tracking-tighter text-Gray500">
-                {lastMessage}
-              </span>
-              <span className="text-[0.8125rem] tracking-tighter text-Gray500">
-                {lastModified}
-              </span>
-            </div>
+    <div className="relative w-[100%+6rem] overflow-hidden">
+      <div
+        className="transition-transform duration-300"
+        style={{
+          transform: isSlided ? "translateX(-2rem)" : "translateX(0)",
+        }}
+      >
+        <div className="flex w-full flex-row gap-2 py-2">
+          <img
+            className="h-14 w-14 flex-shrink-0 rounded-full"
+            src={require("../../assets/images/" + follower.profileImg + ".svg")}
+          />
+          <span className="flex w-full flex-row items-center justify-between gap-[1.88rem]">
+            <span
+              onClick={() => gotoChatRoom()}
+              className="flex flex-grow cursor-pointer flex-col"
+            >
+              <div className="flex flex-row items-center">
+                <span className="text-[0.8125rem] font-semibold tracking-tighter">
+                  {follower.userName}
+                </span>
+                {isPinned && <PinnedIcon className="ml-2 w-3" />}
+              </div>
+
+              <div className="flex flex-row justify-between">
+                <span className="h-[1.219rem] w-[10.187rem] overflow-hidden text-ellipsis whitespace-nowrap text-[0.8125rem] tracking-tighter text-Gray500">
+                  {lastMessage}
+                </span>
+                <span className="text-[0.8125rem] tracking-tighter text-Gray500">
+                  {lastModified}
+                </span>
+              </div>
+            </span>
+            <CameraIcon
+              onClick={clickCamera}
+              className="h-6 w-6 flex-shrink-0 cursor-pointer"
+            />
           </span>
-          <CameraIcon className="h-6 w-6 flex-shrink-0" />
-        </span>
+        </div>
       </div>
-    </>
+      {isSlided && (
+        <span
+          onClick={clickPin}
+          className="absolute bottom-3 right-0 flex h-6 -translate-y-1/2 transform cursor-pointer items-center justify-center"
+        >
+          <PinIcon className="h-8" />
+        </span>
+      )}
+    </div>
   );
 };
 
