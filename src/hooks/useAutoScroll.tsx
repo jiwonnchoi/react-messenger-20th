b249337571fa44ebc51180ce81f98233c0c7c36c
JiwonChoi0805 <won0805@ewhain.net>
2024-11-02 12:19:45 +0900

    fix: 이미지 전송시 자동스크롤 적용 안됨 해결

--- src/hooks/useAutoScroll.tsx
+++ src/hooks/useAutoScroll.tsx
@@ -4,12 +4,15 @@ const useAutoScroll = (dependency: any[]) => {
   const scrollRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    if (scrollRef.current) {
-      scrollRef.current.scrollTo({
-        top: scrollRef.current.scrollHeight, // ref로 전달받은 높이값만큼 top 부여
-        behavior: "smooth",
-      });
-    }
+    const timer = setTimeout(() => {
+      if (scrollRef.current) {
+        scrollRef.current.scrollTo({
+          top: scrollRef.current.scrollHeight, // ref로 전달받은 높이값만큼 top 부여
+          behavior: "smooth",
+        });
+        console.log(scrollRef.current.scrollHeight);
+      }
+    }, 100);
   }, dependency);
 
   return scrollRef;
